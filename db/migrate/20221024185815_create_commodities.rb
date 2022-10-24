5a029c26c9ebd4eb9e67ab33761b9e985d0ad792
Kim <rodrigkim@gmail.com>
2022-10-24 15:02:13 -0400

    updated  migrations

--- db/migrate/20221024185815_create_commodities.rb
+++ db/migrate/20221024185815_create_commodities.rb
@@ -1,4 +1,8 @@
 class CreateCommodities < ActiveRecord::Migration[6.1]
   def change
+    create_table :commodities do |t|
+      t.string :name
+      t.float :price
+    end
   end
 end
--- db/migrate/20221024185822_create_supermarkets.rb
+++ db/migrate/20221024185822_create_supermarkets.rb
@@ -1,4 +1,8 @@
 class CreateSupermarkets < ActiveRecord::Migration[6.1]
   def change
+    create_table :supermarkets do |t|
+      t.string :name
+      t.integer :zipcode
+      t.float :price_index
   end
 end
